75ae3e4ce398eb5694b18c107ef2a711dbd28f89
turuthivic <turuthivictor9@gmail.com>
2017-09-18 21:13:02 +0300

    change migration file naming

--- db/migrate/20170906121822_drop_chat_room_table.rb
+++ db/migrate/20170906121822_drop_chat_room_table.rb
@@ -1,7 +1,7 @@
 class DropChatRoomTable < ActiveRecord::Migration[5.0]
   def change
-  	drop_table :chat_rooms do |t|
-		t.string :topic
+  # 	drop_table :chat_rooms do |t|
+		# t.string :topic
 	end
   end
 end
